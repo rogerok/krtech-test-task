ea8765a997485c7365a49d55efdb322f927b841a
slavik <fedorik83@gmail.com>
2023-02-18 23:41:49 +0300

    created NotFoundPage

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "shared/config/routeConfig";
 import { ChatPage } from "pages/ChatPage/ChatPage";
 import { MainPage } from "pages/MainPage/MainPage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 const AppRouter = () => (
   <Routes>
     <Route path={RoutePath.main} element={<MainPage />}>
       <Route path={`${RoutePath.chat}/:id`} element={<ChatPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   </Routes>
 );
